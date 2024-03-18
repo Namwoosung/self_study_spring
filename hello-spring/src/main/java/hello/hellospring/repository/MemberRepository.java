89b3529a440f4f7eb1177550c635be9f01af7ef0
Namwoosung <namws0023@gmail.com>
2024-03-18 14:35:31 +0900

    for test

--- hello-spring/src/main/java/hello/hellospring/repository/MemberRepository.java
+++ hello-spring/src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -8,8 +8,8 @@ import java.util.Optional;
 //회원 객체를 저장하는 저장소 interface -> 필요한 기능을 정의
 public interface MemberRepository {
     Member save(Member member); //회원 저장
-    Optional<Member> findById(Long id);
-    Optional<Member> findByName(String name);
+    Optional<Member> findById(Long id); //아이디로 검색
+    Optional<Member> findByName(String name); //
     List<Member> findAll(); //모든 회원 조회
 
 }
