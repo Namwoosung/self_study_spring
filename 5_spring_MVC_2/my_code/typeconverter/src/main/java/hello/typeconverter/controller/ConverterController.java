9af96313fffdcfe7653abbe3c6bebe17f38216e1
Namwoosung <namws0023@gmail.com>
2024-06-06 17:34:53 +0900

    Add section 10

--- 5_spring_MVC_2/my_code/typeconverter/src/main/java/hello/typeconverter/controller/ConverterController.java
+++ 5_spring_MVC_2/my_code/typeconverter/src/main/java/hello/typeconverter/controller/ConverterController.java
@@ -23,12 +23,14 @@ public class ConverterController {
         IpPort ipPort = new IpPort("127.0.0.1", 8080);
         Form form = new Form(ipPort);
         model.addAttribute(form);
-        return "converter-form;";
+        return "converter-form";
     }
 
     @PostMapping("converter/edit")
     public String converterEdit(@ModelAttribute Form form, Model model){
-        
+        IpPort ipPort = form.getIpPort();
+        model.addAttribute("ipPort", ipPort);
+        return "converter-view";
     }
 
     @Data
