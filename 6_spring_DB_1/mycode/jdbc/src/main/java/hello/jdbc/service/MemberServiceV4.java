19361a9458a00e8510535fc9f0ed11912054fa23
Namwoosung <namws0023@gmail.com>
2024-06-17 22:39:00 +0900

    Add section 6

---
+++ 6_spring_DB_1/mycode/jdbc/src/main/java/hello/jdbc/service/MemberServiceV4.java
@@ -0,0 +1,47 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepository;
+import hello.jdbc.repository.MemberRepositoryV3;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.SQLException;
+
+/**
+ * 예외 누수 문제 해결
+ * SQLException 제거
+ * <p>
+ * 구현체가 아니라 MemberRepository 인터페이스 의존
+ */
+@Slf4j
+public class MemberServiceV4 {
+
+
+    private final MemberRepository memberRepository;
+
+    public MemberServiceV4(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Transactional
+    public void accountTransfer(String fromId, String toId, int money) {
+        bizLogic(fromId, toId, money);
+    }
+
+    private void bizLogic(String fromId, String toId, int money) {
+        //비즈니스 로직
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(fromId, fromMember.getMoney() - money);
+        validation(toMember); //임의로 예외 상황 발생을 위한 메서드 => toMember가 ex라면 계좌이체 실패라고 가정
+        memberRepository.update(toId, toMember.getMoney() + money);
+    }
+
+    private static void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+}
