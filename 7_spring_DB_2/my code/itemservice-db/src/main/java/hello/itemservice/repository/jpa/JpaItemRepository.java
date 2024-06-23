bf6ebf15537ecd3db450d1f64dc1e0db3d58f72d
Namwoosung <namws0023@gmail.com>
2024-06-23 18:46:46 +0900

    Add section 5

---
+++ 7_spring_DB_2/my code/itemservice-db/src/main/java/hello/itemservice/repository/jpa/JpaItemRepository.java
@@ -0,0 +1,91 @@
+package hello.itemservice.repository.jpa;
+
+import hello.itemservice.domain.Item;
+import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.ItemSearchCond;
+import hello.itemservice.repository.ItemUpdateDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@Repository
+@Transactional //JPA의 모든 변경은 트랜잭션 안에서 이루어짐 => @Transactional을 class레벨에 적용
+public class JpaItemRepository implements ItemRepository {
+
+    //EntityManager가 JPA의 역할
+    //스프링이 EntityManagerFactory를 가지고 datasource를 넣는 등의 작업을 다해서 주입해 줌
+    private final EntityManager em;
+
+    public JpaItemRepository(EntityManager em) {
+        this.em = em;
+    }
+
+    @Override
+    public Item save(Item item) {
+        em.persist(item); //method 하나만 호출하면, Entity 매핑 정보를 바탕으로 sql을 작성하고, db에 저장
+        return item;
+    }
+
+    @Override
+    public void update(Long itemId, ItemUpdateDto updateParam) {
+        Item findItem = em.find(Item.class, itemId);
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
+        // 따로 update에 관련된 method를 수행하지 않아도됨
+        // update 저장을 하지 않아도 JPA가 알아서 트랜잭션이 커밋되는 시점에 SQL을 날림
+    }
+
+    @Override
+    public Optional<Item> findById(Long id) {
+        Item item = em.find(Item.class, id);
+        return Optional.ofNullable(item);
+    }
+
+    @Override
+    public List<Item> findAll(ItemSearchCond cond) {
+        String jpql = "select i from Item i"; //i는 Item entity 자체
+
+        Integer maxPrice = cond.getMaxPrice();
+        String itemName = cond.getItemName();
+
+        if (StringUtils.hasText(itemName) || maxPrice != null) {
+            jpql += " where";
+        }
+
+        boolean andFlag = false;
+        List<Object> param = new ArrayList<>();
+        if (StringUtils.hasText(itemName)) {
+            jpql += " i.itemName like concat('%',:itemName,'%')";
+            param.add(itemName);
+            andFlag = true;
+        }
+
+        if (maxPrice != null) {
+            if (andFlag) {
+                jpql += " and";
+            }
+            jpql += " i.price <= :maxPrice";
+            param.add(maxPrice);
+        }
+
+        log.info("jpql={}", jpql);
+
+        TypedQuery<Item> query = em.createQuery(jpql, Item.class);
+        if (StringUtils.hasText(itemName)) {
+            query.setParameter("itemName", itemName);
+        }
+        if (maxPrice != null) {
+            query.setParameter("maxPrice", maxPrice);
+        }
+        return query.getResultList();
+    }
+}
