97c3af1f8029707e90d8ab708aded59077511993
Namwoosung <namws0023@gmail.com>
2024-07-06 22:54:40 +0900

    Add section 2

--- 8_JPA_basic/my_code/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ 8_JPA_basic/my_code/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -2,15 +2,43 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 public class JpaMain {
 
     public static void main(String[] args) {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
-        //code
 
-        em.close();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try{
+            //회원 저장
+            Member member = new Member();
+            member.setId(1L);
+            member.setName("HelloA");
+
+            em.persist(member);
+
+            //회원 수정, 수정 시에는 따로 저장하지 않아도 commit시점에 알아서 update 쿼리를 날려줌
+            Member findMember = em.find(Member.class, 1L);
+            findMember.setName("HelloJPA");
+
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .getResultList();
+
+            //회원 삭제
+            em.remove(findMember);
+
+            tx.commit();
+        } catch (Exception e){
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+
         emf.close();
     }
 }
