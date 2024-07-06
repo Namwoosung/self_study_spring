4b6ba973750631d4fe1b79e1f1e917b00ec58271
Namwoosung <namws0023@gmail.com>
2024-07-07 02:35:20 +0900

    Add section 3

--- 8_JPA_basic/my_code/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ 8_JPA_basic/my_code/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -15,23 +15,12 @@ public class JpaMain {
         tx.begin();
 
         try{
-            //회원 저장
-            Member member = new Member();
-            member.setId(1L);
-            member.setName("HelloA");
-
+            Member member = new Member(200L, "member200");
             em.persist(member);
 
-            //회원 수정, 수정 시에는 따로 저장하지 않아도 commit시점에 알아서 update 쿼리를 날려줌
-            Member findMember = em.find(Member.class, 1L);
-            findMember.setName("HelloJPA");
+            em.flush();
 
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .getResultList();
-
-            //회원 삭제
-            em.remove(findMember);
-
+            System.out.println("==============");
             tx.commit();
         } catch (Exception e){
             tx.rollback();
