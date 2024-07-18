c1cada79d04788fd0870dd4a7723e9a05396d176
Namwoosung <namws0023@gmail.com>
2024-07-19 02:05:51 +0900

    Add section 9

--- 8_JPA_basic/my_code/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ 8_JPA_basic/my_code/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 import org.hibernate.annotations.ManyToAny;
 
 import java.time.LocalDateTime;
-import java.util.Date;
+import java.util.*;
 
 @Entity
 public class Member {
@@ -15,10 +15,18 @@ public class Member {
     @Column(name = "USERNAME")
     private String username;
 
-    @ManyToOne(fetch = FetchType.EAGER) //find를 해도 Team 객체는 프록시 객체가 반한됨
-    @JoinColumn(name = "TEAM_ID")
-    private Team team;
+    @Embedded
+    private Address homeAddress;
 
+    @ElementCollection // 값 타입 컬렉션으로 지정
+    @CollectionTable(name = "FAVORITE_FOODS", joinColumns = @JoinColumn(name = "MEMBER_ID")) //만들어지는 테이블의 이름, 외래키 등을 지정
+    @Column(name = "FOOD_NAME") //해당 컬렉션 값 타입은 String data하나만 존재 -> 이런경우 예외적으로 컬럼 이름 지정 가능(Address의 경우 임베디드로 따로 클래스가 존재하므로 해당 클래스의 필드 명을 컬럼으로 사용할 것)
+    private Set<String> favoriteFoods = new HashSet<>();
+
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "MEMBER_ID")
+    private List<AddressEntity> addressHistory = new ArrayList<>();
 
     public Long getId() {
         return id;
@@ -36,11 +44,27 @@ public class Member {
         this.username = username;
     }
 
-    public Team getTeam() {
-        return team;
+    public Address getHomeAddress() {
+        return homeAddress;
     }
 
-    public void setTeam(Team team) {
-        this.team = team;
+    public void setHomeAddress(Address homeAddress) {
+        this.homeAddress = homeAddress;
+    }
+
+    public Set<String> getFavoriteFoods() {
+        return favoriteFoods;
+    }
+
+    public void setFavoriteFoods(Set<String> favoriteFoods) {
+        this.favoriteFoods = favoriteFoods;
+    }
+
+    public List<AddressEntity> getAddressHistory() {
+        return addressHistory;
+    }
+
+    public void setAddressHistory(List<AddressEntity> addressHistory) {
+        this.addressHistory = addressHistory;
     }
 }
--- 8_JPA_basic/my_code/ex1-hello-jpa/src/main/java/hellojpa/Team.java
+++ 8_JPA_basic/my_code/ex1-hello-jpa/src/main/java/hellojpa/Team.java
@@ -12,10 +12,6 @@ public class Team {
     @Column(name = "TEAM_ID")
     private Long id;
     private String name;
-
-    @OneToMany(mappedBy = "team")
-    private List<Member> members = new ArrayList<>();
-
     public Long getId() {
         return id;
     }
@@ -32,11 +28,4 @@ public class Team {
         this.name = name;
     }
 
-    public List<Member> getMembers() {
-        return members;
-    }
-
-    public void setMembers(List<Member> members) {
-        this.members = members;
-    }
 }
