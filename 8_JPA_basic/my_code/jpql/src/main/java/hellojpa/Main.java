0cb2b1a509e96313802775ce773418eae9f39e13
Namwoosung <namws0023@gmail.com>
2024-07-28 01:12:00 +0900

    Add section 11

--- 8_JPA_basic/my_code/jpql/src/main/java/hellojpa/Main.java
+++ 8_JPA_basic/my_code/jpql/src/main/java/hellojpa/Main.java
@@ -6,6 +6,7 @@ import hellojpa.jpql.MemberType;
 import hellojpa.jpql.Team;
 import jakarta.persistence.*;
 
+import java.util.Collection;
 import java.util.List;
 
 public class Main {
@@ -18,28 +19,42 @@ public class Main {
         tx.begin();
 
         try{
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            //팀 저장
+            Team teamA = new Team();
+            teamA.setName("teamA");
+            em.persist(teamA);
+            Team teamB = new Team();
+            teamB.setName("teamB");
+            em.persist(teamB);
+            Team teamC = new Team();
+            teamC.setName("teamC");
+            em.persist(teamC);
 
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setAge(10);
-            member.setTeam(team);
-            member.setType(MemberType.ADMIN);
-            em.persist(member);
+            //회원저장
+            Member member1 = new Member();
+            member1.setUsername("회원1");
+            member1.setTeam(teamA);
+            em.persist(member1);
+            Member member2 = new Member();
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
+            em.persist(member2);
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+            em.persist(member3);
+            Member member4 = new Member();
+            member4.setUsername("회원4");
+            em.persist(member4);
 
 
             em.flush();
             em.clear();
 
-            String query = "select coalesce(m.username, '이름 없는 회원') from Member m";
-            String query2 = "select nullif(m.username, 'member1') from Member m";
-            List<String> result = em.createQuery(query, String.class)
-                    .getResultList();
-            for (String s : result) {
-                System.out.println("s = " + s);
-            }
+            String query = "update Member m set m.age = 20";
+            int result = em.createQuery(query)
+                    .executeUpdate();
+            System.out.println("result = " + result);
 
 
             tx.commit();
